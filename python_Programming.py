a82a11b0928e0e1c34d5a25e49dd8a7a05746c6b
panwarankush <ankushpanwar111@gmail.com>
2023-07-15 08:23:36 +0530

    Second Commit

--- python_Programming.py
+++ python_Programming.py
@@ -1,7 +1,7 @@
-name = input('enter your name? ')
+name = input('Enter your name? ')
 
 if len(name) < 3:
-    print('Name Must Be Atleast 3 characters')
+    print('Name Must Be Of Atleast 3 characters')
 elif len(name) > 50:
     print('Name can be a maximum of 50 characters')
 else:
